4fdac33dd5af59969deb866beefeb47734ecb059
Luis Villegas <lualviva97@gmail.com>
2022-06-23 15:08:01 -0500

    Server más endpoints

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+// Require para usar Prisma
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+app.get("/", (req, res) => {
+  res.json({ message: "alive" });
+});
+
+app.get("/v1/explorers", async (req, res) => {
+  const allExplorers = await prisma.explorer.findMany();
+  res.json(allExplorers);
+});
+
+app.get("/v1/explorers/:id", async (req, res) => {
+  const id = req.params.id;
+  const explorer = await prisma.explorer.findUnique({
+    where: { id: parseInt(id) },
+  });
+  res.json(explorer);
+});
+
+app.post("/v1/explorers", async (req, res) => {
+  const explorer = {
+    name: req.body.name,
+    username: req.body.username,
+    mission: req.body.mission,
+  };
+  const message = "Explorer creado.";
+  await prisma.explorer.create({ data: explorer });
+  return res.json(message);
+});
+
+app.put("/v1/explorers/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  await prisma.explorer.update({
+    where: {
+      id: id,
+    },
+    data: {
+      mission: req.body.mission,
+    },
+  });
+  return res.json({ message: "Actualizado correctamente" });
+});
+
+app.delete("/v1/explorers/:id", async (req, res) => {
+  const id = req.params.id;
+  await prisma.explorer.delete({ where: { id: id } });
+  return res.json({ message: "Eliminado correctamente" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
